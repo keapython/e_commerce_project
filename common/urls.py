310c80f83080162ebe9b353eeb0adfac699ce70e
keapython <esenfirdavs1980@gmail.com>
2025-05-23 14:51:20 +0500

    base.html extended for other templates

--- common/urls.py
+++ common/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 
 from common.api_endpoints import *
+from common.views import HomeView, ContactView, ShopDetailsView, ShopingCartView, CheckOutView, BlogDetailsView, ShopGridView, BlogView
 
+app_name = "common"
 
 urlpatterns = [
     path('mediafiles/', MediaFileListAPIView.as_view(), name="mediafile-list"),
     path('mediafiles/create/', MediaFileCreateAPIView.as_view(), name="mediafile-create"),
     path('mediafiles/<int:pk>/update/', MediaFileUpdateAPIView.as_view(), name="mediafile-update"),
     path('mediafiles/<int:pk>/delete/', MediaFileDeleteAPIView.as_view(), name="mediafile-delete"),
+    path('index/', HomeView.as_view(), name="index"),
+    path('contact/', ContactView.as_view(), name="contact"),
+    path('shop-details/', ShopDetailsView.as_view(), name="shop-details"),
+    path('shoping-cart/', ShopingCartView.as_view(), name="shoping-cart"),
+    path('check-out/', CheckOutView.as_view(), name="check-out"),
+    path('blog-details/', BlogDetailsView.as_view(), name="blog-details"),
+    path('shop-grid/', ShopGridView.as_view(), name="shop-grid"),
+    path('blog/', BlogView.as_view(), name="blog"),
 ]
 
--- common/views.py
+++ common/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from products.models import Category
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['title'] = 'Wow-Сommerce | Home'
+        context['categories'] = categories
+        return context
+    
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Contact Us'
+        return context
+    
+
+class ShopDetailsView(TemplateView):
+    template_name = 'shop-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Shop Details'
+        return context
+    
+
+class ShopingCartView(TemplateView):
+    template_name = 'shoping-cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Shopping Cart'
+        return context
+    
+
+class CheckOutView(TemplateView):
+    template_name = 'checkout.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Check Out'
+        return context
+    
+
+class BlogDetailsView(TemplateView):
+    template_name = 'blog-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Blog Details'
+        return context   
+    
+class ShopGridView(TemplateView):
+    template_name = 'shop-grid.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Shop Grid'
+        return context   
+    
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wow-Commerce | Blog'
+        return context  
